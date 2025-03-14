64aea060002bffe2adc60601d07e515a32455914
fruzsi <fruzsi.bognar@gmail.com>
2025-03-14 23:08:09 +0100

    fix product dim

--- sumup/models/core/dimensions/dim_product.sql
+++ sumup/models/core/dimensions/dim_product.sql
@@ -1,10 +1,21 @@
 /* Probably there is also a product raw table 
-but now I just create the dim from transactions
+but now I just create the dim from transactions.
+since some of the skus have different product names and categories
+I will take the latest available value of them
 */
 {{ config(materialized='table') }}
-
-SELECT DISTINCT
+with product_ranking as (
+select 
     product_sku
     , product_name
     , category_name
-FROM {{ref('stg_transaction')}}
+    , row_number() over (partition by product_sku order by transaction_created_at desc) as product_rank_number
+from {{ref('stg_transaction')}}
+)
+-- selecting the latest product name and category by SKU
+select
+    product_sku
+    , product_name
+    , category_name
+from product_ranking
+where product_rank_number=1
