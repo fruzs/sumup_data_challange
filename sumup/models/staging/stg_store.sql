2261b927b1bfa8e5a3237e27d819382c8d0ea71e
fruzsi <fruzsi.bognar@gmail.com>
2025-03-16 11:13:57 +0100

    remove redundant increment in  store

--- sumup/models/staging/stg_store.sql
+++ sumup/models/staging/stg_store.sql
@@ -11,16 +11,3 @@ select
   , customer_id
   , created_at -- to us it for incremental load
 from {{ source('sumup', 'store') }}
-
-{% if is_incremental() %}
-
-  -- this filter will only be applied on an incremental run
-  -- (uses >= to include records whose timestamp occurred since the last run of this model)
-  -- aggregate functions are not allowed in WHERE statements in PG, this is why I chose this solution
-
-where to_timestamp(created_at,'MM/DD/YYYY HH:MI:SS') 
-  >= (select to_timestamp(created_at,'MM/DD/YYYY HH:MI:SS') from {{ this }} 
-     order by 1 desc
-     limit 1)
-
-{% endif %}
